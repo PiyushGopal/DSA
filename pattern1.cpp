59594c492e6f109991e28a19a27d74727dbd701a
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2022-11-02 01:01:40 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,43 @@
+*/
+  example input
+  5
+  
+  example output
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+  */
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count=1;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count=count+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+
+    return 0;
+}
+
+  
